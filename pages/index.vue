88888ba7a8fa2d3e42957ebd45abf77b3a79b60b
Jeffrey <1115378579@qq.com>
2023-03-01 16:22:31 +0800

    feat: 优化svg使用

--- pages/index.vue
+++ pages/index.vue
@@ -5,17 +5,17 @@ import AppInfoBox from 'components/AppInfoBox'
 const line = ref(4)
 </script>
 <template>
-  <div class="app-card" v-for="item, index in data">
+  <div class="app-card" v-for="item in data">
     <AppCardHeader :data="item"/>
     <div class="app-desc" v-html="item.company_content" :style="{'-webkit-line-clamp': line }">
     </div>
-    <a :href="'./details/' + (index + 1)" class="app-more-btn">read more</a>
+    <a :href="'./details/' + item.id" class="app-more-btn">read more</a>
     <AppInfoBox :data="item" />
     <div class="app-testimonial">
       <p>"{{ item.company_short_evaluate }}"</p>
       <p class="app-testimonial-author"> {{ item['company_short_evaluate_name(position)'] }}</p>
     </div>
-    <a :href="'./details/'+ (index + 1)">
+    <a :href="'./details/'+ item.id">
       <div class="app-view-btn">Visit Website</div>
     </a>
   </div>
